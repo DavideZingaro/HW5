95443ddf6a6605f6d7ab65586ea9c8c75c25e8cc
francesco-pez1 <56473264+francesco-pez1@users.noreply.github.com>
2019-12-22 21:49:13 +0100

    Add files via upload

--- func_1.py
+++ func_1.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 15:37:38 2019
+
+@author: franc
+"""
+'''
 import copy
 
 
-def func_1(b,dist):
+def f1(C,b,dist,c):
     # Set of all adjacent nodes
-    c = set()
     a = list(b.keys())[0]
     # C is the graph, b is the current node and dist is the maximum distance
     # from it
-    ric(C,b,dist)
+    ric(C,b,dist,c)
     # Removing initial node
     c.remove(a)
     
     return c
 
-def ric(C,b,dist): #C is the graph
+def ric(C,b,dist,c): #C is the graph
     # Set s to upload the list of nodes whose distance is less than d
     s = set()
     for i in b:
@@ -32,8 +38,49 @@ def ric(C,b,dist): #C is the graph
             if i not in s:
                 del v[i]                
                         
+        if len(s) == 0: # If no node is left to check          
+            return
+        
+        else:            
+            return ric(C,v, dist,c)
+'''
+
+import copy
+
+
+def f1(C,b,dist):
+    # Set of all adjacent nodes
+    c = set()
+    a = list(b.keys())[0]
+    # C is the graph, b is the current node and dist is the maximum distance
+    # from it
+    ric(C,b,dist,c)
+    # Removing initial node
+    c.remove(a)
+    
+    return c
+
+def ric(C,b,dist,c): #C is the graph
+    # Set s to upload the list of nodes whose distance is less than d
+    s = set()
+    for i in b:
+        D = dict(copy.deepcopy(C[i]))
+        for k in D:
+            # Uploading distance
+            D[k]['weight'] += b[i]['weight']
+        
+            # Checking if the distance from b in less than dist
+            if D[k]['weight'] <= dist:
+                c.add(k)
+                s.add(k)
+                
+        v = copy.deepcopy(D)        
+        for i in D:
+            if i not in s:
+                del v[i]                
+                        
     if len(s) == 0: # If no node is left to check          
         return
     
     else:            
-        return ric(C,v, dist)
+        return ric(C,v, dist,c)
